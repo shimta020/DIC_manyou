154d50143c9f638e7b3741d3f778611da77f37fd
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-02-25 15:45:19 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
   resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :sessions, only: [:new, :create, :destroy]
 end
