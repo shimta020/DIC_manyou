9cf243ad82a7f6fed6d9d6173dcea1e176b388ab
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-02-25 14:07:57 +0900

    user作成機能追加

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  before_validation { email.downcase! }
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: {minimum: 6}, on: :create
+
+  has_secure_password
+
+  has_many :tasks
+end
